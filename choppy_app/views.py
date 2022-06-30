ef67995c5da86ccb323c4d023b94bf6001185f43
김영빈 <colock123@gmail.com>
2022-06-30 18:24:50 +0900

    add:error handling

--- choppy_app/views.py
+++ choppy_app/views.py
@@ -1,13 +1,22 @@
+from re import L
 from django.http       import JsonResponse
 from django.views      import View
 from .models           import SupportCompany,Album
 
 class SupportCompanyListView(View):
     def get(self,request):
+        
+        if not SupportCompany.objects.all().exists():
+            return JsonResponse({"error":"support_company doesn't exist"},status=404)
+        
         support_companys = list(SupportCompany.objects.all().values('id','name','images_url'))
         return JsonResponse({"support_companys":support_companys},status=200)
     
 class AlbumListView(View):
     def get(self,request):
+        
+        if not Album.objects.all().exists():
+            return JsonResponse({"error":"Albums doesn't exist"},status=404)
+        
         albums = list(Album.objects.all().values('id','name','singer','images_url','link_url'))
         return JsonResponse({"albums":albums},status=200)
